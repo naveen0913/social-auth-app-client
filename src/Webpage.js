81dafa83597b0a8a012fe809bd2ae352ccf4de78
U Naveen Babu <136116172+naveen0913@users.noreply.github.com>
2023-08-09 19:55:33 +0530

    Update Webpage.js

--- src/Webpage.js
+++ src/Webpage.js
@@ -57,7 +57,7 @@ const App = () => {
   //This function is for First time user signing up with his details
   const handleSubmit=(event)=>{
     event.preventDefault();
-    axios.post('https://social-system.onrender.com/signup',values)
+    axios.post('https://fair-tan-walrus-tutu.cyclic.app/signup',values)
     .then(res=>{
       if(res.status===200){
         alert("signup Succesfull..! login Now ")
@@ -80,14 +80,14 @@ const App = () => {
     if(token){
       localStorage.setItem('authtoken',token);
       setAuthToken(token)
-      window.open("https://social-system.onrender.com/auth/twitter","_self")
+      window.open("https://fair-tan-walrus-tutu.cyclic.app/auth/twitter","_self")
 
     }else{
       console.error("token not found");
     }
 
     localStorage.setItem("userid",userObject?.id)
-    window.open("https://social-system.onrender.com/auth/twitter","_self")    
+    window.open("https://fair-tan-walrus-tutu.cyclic.app/auth/twitter","_self")    
   }
 
   const instagramLogin=()=>{
@@ -104,13 +104,13 @@ const App = () => {
     if(token){
       localStorage.setItem('authtoken',token);
       setAuthToken(token)
-      window.open("https://social-system.onrender.com/auth/linkedin","_self")
+      window.open("https://fair-tan-walrus-tutu.cyclic.app/auth/linkedin","_self")
     }else{
       console.error("token not found");
     }
 
     localStorage.setItem("userid",userObject?.id)
-    window.open("https://social-system.onrender.com/auth/linkedin","_self")
+    window.open("https://fair-tan-walrus-tutu.cyclic.app/auth/linkedin","_self")
     
   }
 
